11f3432d36441e4dbdd2149b94b927caede0ce27
aliaksei <Aliaksei_Hutsalenka@epam.com>
2012-02-10 19:49:16 +0300

    #10 02 2012

--- 0/0.js
+++ 0/0.js
@@ -1,46 +1,68 @@
 /*   XMLHTTPRequest Constructor    */
-function SearchNameq (keyPhrase) {
-    http = null;
-    if (window.XMLHttpRequest) {
-        try {
-            http = new XMLHttpRequest();
-        } catch (e){}
-    } else if (window.ActiveXObject) {
-        try {
-            http = new ActiveXObject('Msxml2.XMLHTTP');
-        } catch (e){
+function XMLHTTP_Request (keyPhrase) {
+    this.phrase = keyPhrase;
+    XMLHTTP_Request.http = null;
+
+    XMLHTTP_Request.prototype.CreateRequest = function(){
+        if (window.XMLHttpRequest) {
             try {
-                http = new ActiveXObject('Microsoft.XMLHTTP');
-            } catch (e){}
+                XMLHTTP_Request.http = new XMLHttpRequest();
+            } catch (e){
+                console.log("Name", e.name, "Message", e.message);
+            }
+        } else if (window.ActiveXObject) {
+            try {
+                XMLHTTP_Request.http = new ActiveXObject('Msxml2.XMLHTTP');
+            } catch (e){
+                try {
+                    XMLHTTP_Request.http = new ActiveXObject('Microsoft.XMLHTTP');
+                } catch (e){
+                    console.log("Name", e.name, "Message", e.message);
+                    //console.exception(e.name, e.message);
+                }
+            }
         }
+        return XMLHTTP_Request.http;
     }
-    if (http){
-        searchUrl = 'http://javascript-training.gametrailers.minsk.epam.com/jstraning/countries.php?q='+keyPhrase;
-        http.onreadystatechange =  function(){ SearchCountry.searchReply(); }
-        http.open('get', searchUrl, true);
-        http.send(null);
-    }else{
-        alert("Браузер не поддерживает AJAX");
+
+    XMLHTTP_Request.prototype.SendRequest = function(){
+        if (XMLHTTP_Request.http){
+            searchUrl = 'http://javascript-training.gametrailers.minsk.epam.com/jstraning/countries.phsp?q='+this.phrase;
+            XMLHTTP_Request.http.onreadystatechange =  function(){ SearchCountry.searchReply(); }
+            XMLHTTP_Request.http.open('get', searchUrl, true);
+            XMLHTTP_Request.http.send(null);
+        }else{
+            alert("Браузер не поддерживает AJAX");
+        }
     }
 }
 
 /* Event listener Constructor */
 function EventListener (el, type, fn) {
-    if (typeof el.addEventListener === 'function') {
-        EventListener = function (el, type, fn) {
-            //alert(el)
-            el.addEventListener(type, fn, false);
-        };
-    } else if (typeof el.attachEvent === 'function') {
-        EventListener = function (el, type, fn) {
-            el.attachEvent('on' + type, fn);
-        };
-    } else {
-        EventListener = function (el, type, fn) {
-            el['on' + type] = fn;
-        };
+    EventListener.el = el;
+    EventListener.type = type;
+    EventListener.fn = fn;
+
+    EventListener.prototype.Create_EventListener = function(){
+        if (typeof EventListener.el.addEventListener === 'function') {
+            EventListener.el.addEventListener(EventListener.type, EventListener.fn, false);
+        } else if (typeof EventListener.el.attachEvent === 'function') {
+            EventListener.el.attachEvent('on' + EventListener.type, EventListener.fn);
+        } else {
+            EventListener.el['on' + EventListener.type] = EventListener.fn;
+            //alert(EventListener.el['on' + EventListener.type]);
+        }
     }
-    return EventListener(el, type, fn);
+
+    EventListener.prototype.Remove_EventListener = function(){
+        if (typeof EventListener.el.removeEventListener === 'function') {
+            EventListener.el.removeEventListener(EventListener.type, EventListener.fn, false);
+        } else if (typeof EventListener.el.detachEvent === 'function') {
+            EventListener.el.detachEvent('on' + EventListener.type, EventListener.fn);
+        } else {
+            //EventListener.el['on' + EventListener.type] = EventListener.fn;
+        }
+    }
 }
 
 /* Key Code Constructor */
@@ -62,7 +84,7 @@ function SearchCountry(word) {
     this.keyword = word;
 
     /* Get element INPUT for handling */
-    this.getElementInput = function(){
+    SearchCountry.prototype.getElementInput = function(){
         var searchInputCollection = document.getElementsByName(this.keyword);       // Collection of objects INPUT
         var searchInputArray = Array.prototype.slice.call(searchInputCollection);   // Conversion to Array
         var searchInput = searchInputArray.shift();                                 // Extraction the first element of the array
@@ -72,10 +94,9 @@ function SearchCountry(word) {
     };
 
     /* Create Element to output the results */
-    this.creatElement = function(tagElement, idElement){
+    SearchCountry.prototype.creatElement = function(tagElement, idElement){
         this.tagElement = tagElement;
         this.idElement = idElement;
-
         var results = document.createElement(this.tagElement);
         results.id = 'search-result-'+this.idElement;
         results.className = 'select-result';
@@ -98,11 +119,16 @@ function SearchCountry(word) {
             if (keyCodeValue == SearchCountry.DOWN) {
                 results.focus();
                 results.getElementsByTagName('option')[0].selected = 'selected';
-                EventListener(results, 'keydown', SearchCountry.listHandler);
+                //EventListener(results, 'keydown', SearchCountry.listHandler);
+                var eventInput = new EventListener(results, 'keydown', SearchCountry.listHandler);
+                eventInput.Create_EventListener();
                 return;
             }
             if (keyCodeValue != SearchCountry.LEFT && keyCodeValue != SearchCountry.UP && keyCodeValue != SearchCountry.RIGHT) {
-                SearchNameq(searchId.value);   // Constructor call XMLHTTPRequest
+                var req = new XMLHTTP_Request(searchId.value);   // Constructor call XMLHTTPRequest
+                req.CreateRequest();
+                req.SendRequest();
+                //var req = new XMLHTTP_Request(searchId.value);   // Constructor call XMLHTTPRequest
             }
         }else if (searchId.value.length < 3){ // if empty area - hide the result
             results.style.display = 'none';
@@ -152,10 +178,10 @@ function SearchCountry(word) {
     };
 
     //  Building a list Method
-    SearchCountry.searchReply = function () {
-        if(http.readyState == 4){
-            if(http.status == 200){
-                var response = eval('('+http.responseText+')');
+    SearchCountry.searchReply = function() {
+        if(XMLHTTP_Request.http.readyState == 4){
+            if(XMLHTTP_Request.http.status == 200){
+                var response = eval('('+XMLHTTP_Request.http.responseText+')');
                 //var response = {"countries":[{"id":20,"iso2":"BY","iso3":"BLR","short_name":"Belarus","long_name":"Republic of Belarus"},{"id":21,"iso2":"BE","iso3":"BEL","short_name":"Belgium","long_name":"Kingdom of Belgium"},{"id":22,"iso2":"BZ","iso3":"BLZ","short_name":"Belize","long_name":"Belize"}]}
                 var listHTML = '';
                 for (var i=0; i<response.countries.length; i++){
@@ -165,15 +191,18 @@ function SearchCountry(word) {
                 results.style.display = "block";
             }
             else {
-                alert("Не удалось получить данные:\n" + http.statusText);
+                console.log("Unable to get data. XMLHTTP_Request.http.status:",XMLHTTP_Request.http.status);
             }
         }
     };
 
     this.init = function(){
-        EventListener(searchId, 'keyup', SearchCountry.inputHandler);    // INPUT handler call
-        EventListener(results, 'click', SearchCountry.listHandler);      // Parsing of results list
-        EventListener(results, 'click', SearchCountry.clickHandler);     // Mouse click event
+        var eventInput = new EventListener(searchId, 'keyup', SearchCountry.inputHandler);      // INPUT handler call
+        eventInput.Create_EventListener();
+        var eventList = new EventListener(results, 'click', SearchCountry.listHandler);         // Parsing of results list
+        eventList.Create_EventListener();
+        var eventMauseClick = new EventListener(results, 'click', SearchCountry.clickHandler);  // Mouse click event
+        eventMauseClick.Create_EventListener();
     }
 
     this.init();
