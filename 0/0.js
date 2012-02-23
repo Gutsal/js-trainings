cff44ed34b98a722c2bd870446cdf7758c7aa96f
aliaksei <Aliaksei_Hutsalenka@epam.com>
2012-02-23 19:14:44 +0300

    #23 02 2012

--- 0/0.js
+++ 0/0.js
@@ -1,15 +1,17 @@
 /*   XMLHTTPRequest Constructor    */
 function XmlHttpRequest (KeyPhrase){
-    XmlHttpRequest.prototype.Phrase = KeyPhrase;
-    XmlHttpRequest.prototype.Http = null;
-    XmlHttpRequest.prototype.Ready = 4;
-    XmlHttpRequest.prototype.Status = 200;
+    XmlHttpRequest.prototype.options = {
+        Phrase : KeyPhrase,
+        Http : null,
+        Ready : 4,
+        Status : 200
+    }
 }
 
 XmlHttpRequest.prototype.Create = function(){
     if (window.XMLHttpRequest){
         try {
-            XmlHttpRequest.prototype.Http = new XMLHttpRequest();
+            this.options.Http = new XMLHttpRequest();
         }
         catch (e){
             console.log("Name", e.name, "Message", e.message);
@@ -17,36 +19,35 @@ XmlHttpRequest.prototype.Create = function(){
     }
     else if (window.ActiveXObject){
         try {
-            XmlHttpRequest.prototype.Http = new ActiveXObject('Msxml2.XMLHTTP');
+            this.options.Http = new ActiveXObject('Msxml2.XMLHTTP');
         }
         catch (e){
             try {
-                XmlHttpRequest.prototype.Http = new ActiveXObject('Microsoft.XMLHTTP');
+                this.options.Http = new ActiveXObject('Microsoft.XMLHTTP');
             }
             catch (e){
                 console.log("Name", e.name, "Message", e.message);
-                //console.exception(e.name, e.message);
             }
         }
     }
-    return XmlHttpRequest.prototype.Http;
+    return this.options.Http;
 }
 
 XmlHttpRequest.prototype.Send = function(Results){
-    if (XmlHttpRequest.prototype.Http){
-        SearchUrl = Autocomplete.prototype.Url+XmlHttpRequest.prototype.Phrase;
-        XmlHttpRequest.prototype.Http.onreadystatechange =  function(){
-            if (XmlHttpRequest.prototype.Http.readyState == XmlHttpRequest.prototype.Ready){
-                if (XmlHttpRequest.prototype.Http.status == XmlHttpRequest.prototype.Status){
+    if (this.options.Http){
+        SearchUrl = Autocomplete.prototype.options.Url+this.options.Phrase;
+        this.options.Http.onreadystatechange = function(){
+            if (XmlHttpRequest.prototype.options.Http.readyState == XmlHttpRequest.prototype.options.Ready){
+                if (XmlHttpRequest.prototype.options.Http.status == XmlHttpRequest.prototype.options.Status){
                     Autocomplete.SearchReply(Results);
                 }
                 else {
-                    console.log("Unable to get data. XMLHTTP_Request.http.status:",XmlHttpRequest.prototype.Http.status);
+                    console.log("Unable to get data. XMLHTTP_Request.http.status:",XmlHttpRequest.prototype.options.Http.status);
                 }
             }
         }
-        XmlHttpRequest.prototype.Http.open('get', SearchUrl, true);
-        XmlHttpRequest.prototype.Http.send(null);
+        this.options.Http.open('get', SearchUrl, true);
+        this.options.Http.send(null);
     }
     else {
         console.log("Your browser does not support AJAX");
@@ -105,29 +106,31 @@ Autocomplete.prototype.CreateElement = function(TagElement, IterationElement, Se
 /* Search Request Constructor */
 function Autocomplete(data){
 
-    Autocomplete.prototype.Keyword = data.Word;
-    Autocomplete.prototype.Category = data.Category;
-    Autocomplete.prototype.Url = data.Url;
-    Autocomplete.prototype.Tab = 9;
-    Autocomplete.prototype.Left = 37;
-    Autocomplete.prototype.Up = 38;
-    Autocomplete.prototype.Right = 39;
-    Autocomplete.prototype.Down = 40;
-    Autocomplete.prototype.Enter = 13;
-    Autocomplete.prototype.SearchCollection = document.getElementsByClassName(Autocomplete.prototype.Keyword);         // Element INPUT initialization
+    Autocomplete.prototype.options = {
+        SearchClassName : document.getElementsByClassName(data.ClassName),
+        Category : data.Category,
+        Url : data.Url,
+        Tab : 9,
+        Left : 37,
+        Up : 38,
+        Right : 39,
+        Down : 40,
+        Enter : 13
+    }
 
-    for (i = 0; i < Autocomplete.prototype.SearchCollection.length; i++){
-        var SearchId = Autocomplete.prototype.SearchCollection[i];                         // Element INPUT initialization
+    for (i = 0; i < this.options.SearchClassName.length; i++){
+        var SearchId = this.options.SearchClassName[i];                         // Element INPUT initialization
         var Results = this.CreateElement('select',i,SearchId);      // Result Element initialization
     }
 
     /* input handler */
     Autocomplete.InputHandler = function (e){
+        //alert(this);
         var KeyCodeValue = GetKeyCode(e);
             Results = this.nextElementSibling;
             Results.selectedIndex = -1;
-            if (this.value.length > 2 && KeyCodeValue != Autocomplete.prototype.Enter){
-                if (KeyCodeValue == Autocomplete.prototype.Down){
+            if (this.value.length > 2 && KeyCodeValue != Autocomplete.prototype.options.Enter){
+                if (KeyCodeValue == Autocomplete.prototype.options.Down){
                     Results.focus();
                     Results.getElementsByTagName('option')[0].selected = 'selected';
                     var EventInput = new EventListener(Results, 'keydown', Autocomplete.ListHandler);
@@ -136,7 +139,7 @@ function Autocomplete(data){
                     EventMouseClick.Create();
                     return;
                 }
-                if (KeyCodeValue != Autocomplete.prototype.Left && KeyCodeValue != Autocomplete.prototype.Up && KeyCodeValue != Autocomplete.prototype.Right && KeyCodeValue != Autocomplete.prototype.Tab){
+                if (KeyCodeValue != Autocomplete.prototype.options.Left && KeyCodeValue != Autocomplete.prototype.options.Up && KeyCodeValue != Autocomplete.prototype.options.Right && KeyCodeValue != Autocomplete.prototype.options.Tab){
                     var Req = new XmlHttpRequest(this.value);   // Constructor call XmlHttpRequest
                     Req.Create();
                     Req.Send(Results);
@@ -151,14 +154,14 @@ function Autocomplete(data){
     Autocomplete.ListHandler = function(e){
         var KeyCodeValue = GetKeyCode(e);
         var Current = parseInt(this.id.replace(/select-result-/,''));
-        for (i = 0; i < Autocomplete.prototype.SearchCollection.length; i++){
+        for (i = 0; i < Autocomplete.prototype.options.SearchClassName.length; i++){
             if (Current == i){
-                SearchId = Autocomplete.prototype.SearchCollection[i];
+                SearchId = Autocomplete.prototype.options.SearchClassName[i];
             }
         }
         if (KeyCodeValue){
             // if "Enter" key
-            if (KeyCodeValue == Autocomplete.prototype.Enter){
+            if (KeyCodeValue == Autocomplete.prototype.options.Enter){
                 SearchId.value = this[this.selectedIndex].text;
                 this.selectedIndex = -1;
                 this.style.display = 'none';
@@ -173,13 +176,13 @@ function Autocomplete(data){
                 return;
             }
             // if "Up Arrow" key
-            if (KeyCodeValue == Autocomplete.prototype.Up){
+            if (KeyCodeValue == Autocomplete.prototype.options.Up){
                 if (this.selectedIndex == 0){
                     SearchId.focus();
                 }
             }
             // if choose the last
-            if (KeyCodeValue == Autocomplete.prototype.Down){
+            if (KeyCodeValue == Autocomplete.prototype.options.Down){
                 if (this.selectedIndex == this.length-1) {
                     this.selectedIndex = -1;
                 }
@@ -190,9 +193,9 @@ function Autocomplete(data){
     /* Mouse click handler */
     Autocomplete.ClickHandler = function(e){
         var Current = parseInt(this.id.replace(/select-result-/,''));
-        for (i = 0; i < Autocomplete.prototype.SearchCollection.length; i++){
+        for (i = 0; i < Autocomplete.prototype.options.SearchClassName.length; i++){
             if (Current == i){
-                SearchId = Autocomplete.prototype.SearchCollection[i];
+                SearchId = Autocomplete.prototype.options.SearchClassName[i];
             }
         }
         SearchId.value = this[this.selectedIndex].text;
@@ -203,8 +206,8 @@ function Autocomplete(data){
 
     //  Building a list Method
     Autocomplete.SearchReply = function(Results) {
-        var Response = JSON.parse(XmlHttpRequest.prototype.Http.responseText);
-        Response = Response[Autocomplete.prototype.Category];
+        var Response = JSON.parse(XmlHttpRequest.prototype.options.Http.responseText);
+        Response = Response[Autocomplete.prototype.options.Category];
         //var response = {"countries":[{"id":20,"iso2":"BY","iso3":"BLR","short_name":"Belarus","long_name":"Republic of Belarus"},{"id":21,"iso2":"BE","iso3":"BEL","short_name":"Belgium","long_name":"Kingdom of Belgium"},{"id":22,"iso2":"BZ","iso3":"BLZ","short_name":"Belize","long_name":"Belize"}]}
         Results.innerHTML = '';
         for (var i=0; i<Response.length; i++){
@@ -217,23 +220,23 @@ function Autocomplete(data){
             Results.style.display = "block";
         }
         else {
-            console.log("Result is empty. "+Autocomplete.prototype.Category+': '+Response.length);
+            console.log("Result is empty. "+Autocomplete.prototype.options.Category+': '+Response.length);
         }
 
     };
 
     this.Init = function(){
-        var EventInput = new EventListener(Autocomplete.prototype.SearchCollection[i], 'keyup', Autocomplete.InputHandler);      // INPUT handler call
+        var EventInput = new EventListener(this.options.SearchClassName[i], 'keyup', Autocomplete.InputHandler);      // INPUT handler call
         EventInput.Create();
     }
 
-    for (i = 0; i < Autocomplete.prototype.SearchCollection.length; i++){
+    for (i = 0; i < this.options.SearchClassName.length; i++){
         this.Init(i);
     }
 }
 
 var search = new Autocomplete({
-    Word:'keyword',
-    Category:'countries',
-    Url:'http://javascript-training.gametrailers.minsk.epam.com/jstraning/countries.php?q='
+    ClassName : 'keyword',
+    Category : 'countries',
+    Url : 'http://javascript-training.gametrailers.minsk.epam.com/jstraning/countries.php?q='
 });
